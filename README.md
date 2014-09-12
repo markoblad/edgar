b6bd4a77a68c4dd7095eb0b295ebefa5d2a9c6df
Mark Oblad <mark.oblad@gmail.com>
2014-09-11 22:43:50 -0400

    initial rails commit

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a simple rails 3.2 app for grabbing and parsing XBRL files (and a few others) from the SEC's edgar site.
+
+A few useful commands:
+
+- to clone the repo from github:
+git clone https://github.com/markoblad/edgar.git
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edgar::Application.config.secret_token = 'b3b00892aca36a3d2403ddc25a4b3d0fe6f7a02773ec963a119e061b12c40e2710857485937995ff68fd1c9ddb68d5e704466d8f2415760fa3c2b3204798c44d'
